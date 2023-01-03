5383b10558bb80933d73cc9a9dc37fe464bafd8b
TharinduKahawala <u1123036@umail.usq.edu.au>
2023-01-03 14:03:08 +1000

    Day 27 Send email

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 api_key = '4a28d0323aa04807a81acdbee0732d44'
 url = "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=4a28d0323aa04807a81acdbee0732d44"
@@ -8,7 +9,16 @@ request = requests.get(url)
 # Get a dictionary with data
 content = request.json()
 
-#Access the article titles and description
+body = " "
+# Access the article titles and description
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    if article["ttile"] is not None:
+        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+
+body = body.encode('utf-8')
+
+# Send email
+send_email(body)
+
+print("Email has been sent")
+
